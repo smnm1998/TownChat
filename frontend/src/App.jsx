0388ca49d74527c9beaa8a485ab1888293d8708b
smnm1998 <dltpals7498@gmail.com>
2025-03-10 08:30:23 +0900

    feat: route 추가

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,22 @@
-import './App.css';
+import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
+import './App.css'
+import SignIn from '@pages/Auth/SignIn';
+import SignUp from '@pages/Auth/SignUp';
 
 function App() {
-    return <>HelloWorld!</>;
+    return (
+        <Router>
+            <Routes>
+                <Route path="/signin" element={<SignIn />} />
+                
+                <Route path="/signup" element={<SignUp />} />
+                
+                <Route path="/login" element={<Navigate to="/signin" replace />} />
+                <Route path="/" element={<Navigate to="/signin" replace />} />
+                <Route path="*" element={<Navigate to="/signin" replace />} />
+            </Routes>
+        </Router>
+    );
 }
 
 export default App;
