671dc0b604d2b8b2f22f693ae597ca1e6aabdaf6
smnm1998 <dltpals7498@gmail.com>
2025-03-18 20:14:02 +0900

    build: 백엔드 연동 시 포트 충돌 해결

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -24,8 +24,8 @@ export default defineConfig({
             '/api': {
                 target: 'http://localhost:3000',
                 changeOrigin: true,
-                rewrite: (path) => path.replace(/^\/api/, ''),
-            },
-        },
-    },
+                secure: false
+            }
+        }
+    }
 });
